23b828227ff95a12b225594adfd5f6027a371830
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2026-03-14 12:14:51 +0100

    style: switch to higher contrast gruvbox-dark-hard highlighting

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import hljs from 'highlight.js/lib/core';
 import python from 'highlight.js/lib/languages/python';
 import bash from 'highlight.js/lib/languages/bash';
-import 'highlight.js/styles/base16/gruvbox-dark-medium.css';
+import 'highlight.js/styles/base16/gruvbox-dark-hard.css';
 import './theme/theme.scss';
 import './dynamics.js';
 
