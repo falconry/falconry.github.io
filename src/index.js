2ef26dba9349f07cc70f6829f0f929eedc38f62e
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2026-03-14 22:40:22 +0100

    chore(deps): upgrade dependencies, address vulnerabilities (#404)
    * chore(deps): upgrade dependencies, address vulnerabilities
    
    * chore: upgrade to bootstrap 5 (wip)
    
    * chore: fix layout issues after bootstrap 5 upgrade
    
    * chore: upgrade highlight.js to v11
    
    * style: switch highlighting to gruvbox-dark-medium
    
    * style: switch to higher contrast gruvbox-dark-hard highlighting
    
    * style: fix mobile hamburger menu appearance
    
    * style: address SCSS deprecation warnings by switching to @use
    
    * Revert "style: address SCSS deprecation warnings by switching to @use"
    
    This reverts commit 2e8d4e6ff75d345b230aa34ac2116ae2e4e96522.
    
    * style: iteratively address SCSS deprecations

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import 'bootstrap';
+import 'bootstrap/dist/js/bootstrap.esm';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import hljs from 'highlight.js/lib/core';
 import python from 'highlight.js/lib/languages/python';
 import bash from 'highlight.js/lib/languages/bash';
-import 'highlight.js/styles/railscasts.css';
+import 'highlight.js/styles/base16/gruvbox-dark-hard.css';
 import './theme/theme.scss';
 import './dynamics.js';
 
